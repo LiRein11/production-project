b2875fa9776012e140bb6572e804b3c83e44191d
Stepanov Sergey <sergey11200011@mail.ru>
2023-05-27 14:31:43 +0500

    fix storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
 
-    config.resolve.modules.push(paths.src);
+    config.resolve.modules.unshift(paths.src);
     config.resolve.extensions.push('.ts', '.tsx');
 
     // eslint-disable-next-line
