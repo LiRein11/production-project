cf7724a6365e395f95c463e0fdf591a837e1038f
Stepanov Sergey <sergey11200011@mail.ru>
2023-05-27 14:43:52 +0500

    fix screens

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,3 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
@@ -8,7 +6,7 @@ const MainPage = () => {
     return (
         <>
             <div>{t('Главная страница')}</div>
-            <Counter />
+            {/* <Counter /> */}
             {/* <BugButton /> */}
         </>
     );
