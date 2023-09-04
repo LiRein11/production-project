a6b9d1ced69aa9cab0faf9d9002694108d860f5e
Stepanov Sergey <sergey11200011@mail.ru>
2023-09-04 22:54:36 +0500

    fix

--- src/pages/ArticlePageDetails/ui/AdditionalInfoContainer/AdditionalInfoContainer.stories.tsx
+++ src/pages/ArticlePageDetails/ui/AdditionalInfoContainer/AdditionalInfoContainer.stories.tsx
@@ -25,7 +25,7 @@ const article = {
     user: {
         id: '1',
         username: 'LiRein',
-        avatar: 'https://catherineasquithgallery.com/uploads/posts/2021-02/1612257690_85-p-paren-na-fioletovom-fone-111.jpg',
+        avatar: 'https://pristor.ru/wp-content/uploads/2019/09/%D0%9F%D1%80%D0%B8%D0%BA%D0%BE%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-%D0%BD%D0%B0-%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80%D0%BA%D1%83-%D0%B4%D0%BB%D1%8F-%D0%BC%D1%83%D0%B6%D1%87%D0%B8%D0%BD-3.jpg',
     },
     type: ['IT', 'SCIENCE', 'ECONOMICS'],
     blocks: [
